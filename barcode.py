36ec61bbbd316bb7945e293a5c73472a10b0369f
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-07-30 18:08:56 +0530

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -12,7 +12,7 @@ file_handle = fitz.open(input_file)
 first_page = file_handle[0]
 
 # add the image
-first_page.insertImage(image_rectagle,filename=barcode,)
+first_page.insertImage(image_rectagle,filename=barcode,overlay=True,)
 
 file_handle.save(output_file)
 
