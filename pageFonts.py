7880de907f4489c129db411a604a58df317f405e
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-08-15 18:37:48 +0530

    Add files via upload

--- pageFonts.py
+++ pageFonts.py
@@ -1,15 +1,28 @@
 import fitz
+from functools import reduce
+
+inputp="abc.pdf"
+pdf=fitz.open(inputp)
+
+count=len(pdf)
+print(count)
 
 print("1. Page No.\n2. Complete PDF")
 n=int(input())
 
-inputp="pymupdf.pdf"
-pdf=fitz.open(inputp)
+font=[]
 
 if n==1:
     no=int(input("Page number: "))
-    page=no
+    page=[page[3] for page in pdf.getPageFontList(no, full=False)]
+    font.append(page)
 
-for page in pdf.getPageFontList(0,full=False):
-    print(page[3])
+else:
+    for i in range(0,count):
+        page=[page[3] for page in pdf.getPageFontList(i,full=False)]
+        font.append(page)
 
+fonts=reduce(lambda x,y:x+y,font)
+
+for i in set(fonts):
+    print((i.split("+")[1]))
