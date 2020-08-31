58e00d2434870172c429075997a241712fa6d5d5
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-08-31 17:18:09 +0530

    Update deletePage.py

--- deletePage.py
+++ deletePage.py
@@ -1,14 +1,18 @@
 import fitz
 
 inputp="pdf_name.pdf"
+# open the pdf 
 pdf=fitz.open(inputp)
 
+# total number of pages in the pdf
 count=len(pdf)
 print("Total pages:",count)
 
+# enter the page no. to delete
 pno = int(input("Page number: "))
 
 com=pdf.deletePage(pno)
 
+# output is saved in a new file "Updated_pdf_name.pdf"
 output="Updated_"+inputp
 pdf.save(output)
