ab9bc408d94f288046d231f58a5ff2a5f8e4d66f
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-08-31 17:11:57 +0530

    Update PDF2Image.py

--- PDF2Image.py
+++ PDF2Image.py
@@ -4,7 +4,8 @@ pdf_name='pdf_name.pdf'
 
 pdf=fitz.open(pdf_name)
 
+# Reads one page at a time 
 for page in pdf:
-    pix=page.getPixmap(alpha=True)
-    pix.writePNG("page-%i.png" %page.number)
+    pix=page.getPixmap(alpha=True)   
+    pix.writePNG("page-%i.png" %page.number)  # page converted to PNG format
     
