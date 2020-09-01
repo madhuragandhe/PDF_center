f89ba9fedcc6b5e020a5e2f3809cbae6056923b7
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-01 15:00:23 +0530

    Update doc2pdf.py

--- doc2pdf.py
+++ doc2pdf.py
@@ -1,12 +1,17 @@
 import fitz
 
-input="abc.docx"
+input="pdf_name.docx"
 out=list(input.split("."))[0]
-output=out+".pdf"
+output=out+".pdf"  
 
+# Open the pdf
 con=fitz.open(input)
 
+# Convert to pdf, -1 indicates the start page to end page
 pdfbytes=con.convertToPDF(from_page=-1,to_page=-1,rotate=0)
 
+# Open the pdfbytes in the pdf format
 pdf=fitz.open("pdf",pdfbytes)
+
+# Save it in desired output file
 pdf.save(output)
