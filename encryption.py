24b1e8661197edd937b0b073380a8fd3467c9f43
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-02 17:54:31 +0530

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,18 +1,24 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 
 inputf='pdf_name.pdf'
-output=inputf+'_encrypt.pdf'
+output='encrypt_'+inputf
 
+# Ask the user for the password
 password=input("Enter password")
 
+# Open the input file in read mode
 PDFfile=open(inputf,"rb")
 PDFinput=PdfFileReader(PDFfile)
 PDFoutput=PdfFileWriter()
 
 for page in range(PDFinput.numPages):
+    # For each page of the input pdf add the page to the output pdf
     PDFoutput.addPage(PDFinput.getPage(page))
 
+# Encrypt the contents
 PDFoutput.encrypt(password)
+# Open the output file in write mode
 result=open(output,"wb")
+# Write the contents with encryption
 PDFoutput.write(result)
 result.close()
