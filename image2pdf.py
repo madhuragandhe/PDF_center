9a9f07abcf2a0078463a26c8acc7726e8f373e83
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-03 20:13:40 +0530

    Update image2pdf.py

--- image2pdf.py
+++ image2pdf.py
@@ -4,9 +4,13 @@ pdf=fitz.open()
 img_list=['img.png','bar.png']
 
 for img in img_list:
+    # Open each image from the image list
     imgdoc=fitz.open(img)
+    # Convert the opened image to PDF
     pdfbytes=imgdoc.convertToPDF()
     imgpdf=fitz.open("pdf",pdfbytes)
+    # Insert the converted pdf to the original pdf
     pdf.insertPDF(imgpdf)
 
+# Save the new pdf
 pdf.save("ImagesPDF.pdf")
