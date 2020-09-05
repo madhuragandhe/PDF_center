a221ffe18f89ada936ebddda17aae9c001f4ae6a
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-05 16:18:10 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -2,8 +2,11 @@ from PyPDF2 import PdfFileReader
 
 file="pdf_name.pdf"
 
+# Open the file in read mode
 output_reader=PdfFileReader(open(file,"rb"))
+# Get the pdf's information/metadata
 info=output_reader.getDocumentInfo()
 
+# Print each information 
 for key in info:
     print(key[1:]," --> ",info[key])
