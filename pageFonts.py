35c39edb1e94815adc601f6fe05c6d9e3f70ab24
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-06 14:50:10 +0530

    Update pageFonts.py

--- pageFonts.py
+++ pageFonts.py
@@ -1,9 +1,11 @@
 import fitz
 from functools import reduce
 
-inputp="abc.pdf"
+inputp="pdf_name.pdf"
+# Open the pdf
 pdf=fitz.open(inputp)
 
+# Count the number of pages
 count=len(pdf)
 print(count)
 
@@ -14,15 +16,18 @@ font=[]
 
 if n==1:
     no=int(input("Page number: "))
+    # Get the font for the particular page
     page=[page[3] for page in pdf.getPageFontList(no, full=False)]
     font.append(page)
 
 else:
     for i in range(0,count):
+        # For each page in the PDF, get the font of the page
         page=[page[3] for page in pdf.getPageFontList(i,full=False)]
         font.append(page)
 
 fonts=reduce(lambda x,y:x+y,font)
 
 for i in set(fonts):
+    # Print different fonts in the set 
     print((i.split("+")[1]))
