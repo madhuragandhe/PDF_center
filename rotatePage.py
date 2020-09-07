cc26e70c23d6ba3e6c717d291e57188bfac775c9
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-07 18:21:02 +0530

    Update rotatePage.py

--- rotatePage.py
+++ rotatePage.py
@@ -1,10 +1,13 @@
 import fitz
 
-inputp='abc.pdf'
+inputp='pdf_name.pdf'
+# Open the pdf 
 pdf=fitz.open(inputp)
 
 for page in pdf:
+    # For each page in the PDF rotate the page by 90 degree to the right
     page.setRotation(90)
 
+# Save the updated pdf in the output file 
 output="Rotated_"+inputp
 pdf.save(output)
