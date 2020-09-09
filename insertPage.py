b2dd3381b0c4dd377b43816eb33ebf5807d3bfc5
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-09-09 16:58:34 +0530

    Update insertPage.py

--- insertPage.py
+++ insertPage.py
@@ -1,8 +1,10 @@
 import fitz
 
 inputp="pdf_name.pdf"
+# Open the pdf
 pdf=fitz.open(inputp)
 
+# Count the number of pages
 count=len(pdf)
 print("Total pages:",count)
 
@@ -11,12 +13,17 @@ n=int(input())
 
 pno = int(input("Page number [-1 for first page] : "))
 
+
 if n==2:
+    # Ask th euser to enter the text
     text=input("Enter text: \n")
+    # Insert a new page on the mentioned pageNo with user entered text written
     com=pdf.insertPage(pno,text)
 
 else:
+    # Insert a new page on the user entered pageNo
     com=pdf.newPage(pno)
 
+# Save the updated changes in new output file    
 output="Updated_"+inputp
 pdf.save(output)
